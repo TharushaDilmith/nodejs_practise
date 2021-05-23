09a82e09e1af56d2a6f01cf7361ca69bea104183
IT19153964 <tharushadilmith99@gmail.com>
2021-05-24 01:09:12 +0530

    first commit

--- app.js
+++ app.js
@@ -386,9 +386,13 @@
 
 //partial ejs
 var express = require('express');
+var bodyParser = require('body-parser');
 var app= express();
 app.set('view engine','ejs');
 app.use('/styles',express.static('styles'));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 app.get('/',function (req,res) {
     
@@ -396,7 +400,13 @@ app.get('/',function (req,res) {
 });
 app.get('/contact',function (req,res) {
     res.render('contact',{qs:req.query});
-})
+});
+
+app.post('/contact', urlencodedParser, function (req,res) {
+    console.log(req.body);
+    res.render('contact',{qs:req.query});
+});
+
 app.get('/profile/:name',function (req,res) {
     var data = {age:29, town:'Horana',hobbies:['watching','playing','eating','coding','sleeping']};
     res.render('profile',{person:req.params.name,data:data});  
